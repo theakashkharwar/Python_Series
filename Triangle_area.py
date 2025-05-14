ab31bab55daa04773a78495d9d3a77ad19410198
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-14 14:17:29 +0530

    Area of a Triangle

---
+++ Triangle_area.py
@@ -0,0 +1,11 @@
+# Find The ARea of a Triangle
+
+Height = float(input("Enter The Height of a triangle: "))
+
+Base = float(input("Enter The Base of a triangle: "))
+
+print("Area of triangle is: ", 0.5 * Height * Base)
+
+
+
+
