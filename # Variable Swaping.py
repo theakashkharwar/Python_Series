dc14946587a7704174920df2bc909f865cc96be7
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-14 14:32:01 +0530

    Variable Swapping

---
+++ # Variable Swaping.py
@@ -0,0 +1,20 @@
+# Variable Swaping
+
+# Enter the Variables 
+Var_1 = input("Enter the Value of A: \n")
+Var_2 = input("Enter the Value of B: \n")
+
+# Print the original Value or value before swapping
+
+print("Show the values of the variables before swapping \n A: " + Var_1 + "\n B: " + Var_2)
+
+# Swapping
+temp = Var_1
+Var_1 = Var_2
+Var_2 = temp
+
+# Values after Swapping
+print("Show the Values of the variable after Swapping \n A:" + Var_1 + "\n B: " + Var_2)
+
+
+
