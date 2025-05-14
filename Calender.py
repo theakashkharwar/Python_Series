f2e19dca338c6e5ca1535f38c12d556a9dd005dd
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-14 15:00:38 +0530

    Calender and Months

---
+++ Calender.py
@@ -0,0 +1,15 @@
+# Task: Show the month of the year
+
+#Import Calender Module
+import calendar
+
+# Take the input of the year and month
+Year = int(input("Tell me which year u want to see:\n "))
+Month = int(input(f"Tell which month of the year {Year} u want to see:\n "))
+
+# Store the Values in a Variable
+
+Cal = calendar.month(Year, Month)
+
+print(Cal)
+
