588a294c226eb23a977a3128398233def471c7ef
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-14 14:47:02 +0530

    Conversion Km to Miles

---
+++ Conversion_(Km_to_miles).py
@@ -0,0 +1,11 @@
+# Task : Convert Kilometers to miles
+
+# Enter the Value in Kilometers
+Km = float(input("Enter the Value in Km: "))
+
+# Convert the Value in Miles
+Miles = Km * 0.62137119
+
+# Print the value in Miles 
+print(f"\nValue in Miles is: {Miles}")
+
