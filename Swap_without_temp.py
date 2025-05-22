e49dd13ef524a6e63cc0d9103adbb8a3daf1ca98
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-22 16:30:06 +0530

    Swapping without temp

---
+++ Swap_without_temp.py
@@ -0,0 +1,16 @@
+# Task: To Swap no. without using temp
+
+# Creating Variables
+a = 10
+b = 23
+
+# Print the no. before swaping
+print(f"Variables befor swapping are- a: {a} and b: {b}")
+
+# After Swapping
+a, b = b, a
+
+# Print the no after swapping
+print(f"Variables after swapping ar a: {a} and b: {b}")
+
+
