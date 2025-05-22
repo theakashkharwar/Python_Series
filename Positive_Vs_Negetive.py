f962af98a20efaee78e1925e5b3a94236a0ddd16
Akash Kharwar <akashkharwarai@outlook.com>
2025-05-22 16:36:43 +0530

    Program to check if the no. is postive, negetive or zero

---
+++ Positive_Vs_Negetive.py
@@ -0,0 +1,11 @@
+# Task : To check if the no. is positive, negetive or zero
+num = float(input("Enter the NO.: \n"))
+
+if num > 0:
+    print("The num is Positive.")
+elif num < 0:
+    print("The num is Negetive.")
+else:
+    print("The no. is Zero.")
+
+
